695c6baff4fedadb9c5100b278f7b41a6c9e7c44
keskinsam <keskinsam@gmail.com>
2018-02-02 14:29:57 +0300

    ListView

--- app/src/main/java/com/smtgroup/estateapplication/HomepageHouseCategory.java
+++ app/src/main/java/com/smtgroup/estateapplication/HomepageHouseCategory.java
@@ -3,6 +3,8 @@ package com.smtgroup.estateapplication;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,23 +14,79 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomepageHouseCategory extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    @BindView(R.id.listHouse)
+    ListView listHouse;
+    LayoutInflater linf;
+    BaseAdapter adp;
 
 
+    List ls = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_house_category);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        dataGetir();
+        Log.d("Sizeee :",  ""+ls.size());
+        linf = LayoutInflater.from(this);
+
+
+        ButterKnife.bind(this);
+
+        adp = new BaseAdapter() {
+            @Override
+            public int getCount() {
+
+                return ls.size();
+
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View view, ViewGroup viewGroup) {
+                if (view == null) {
+                    view = linf.inflate(R.layout.house_row, null);
+                }
+
+                try {
+                    TextView txtKategori = view.findViewById(R.id.txtHouseCategory);
+                    txtKategori.setText(ls.get(position).toString());
+                }catch (Exception e) {
+                    Log.e("Liste doldurma hatası",e.toString());
+                }
+                return view;
+            }
+        };
+        listHouse.setAdapter(adp);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -104,5 +162,19 @@ public class HomepageHouseCategory extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public List dataGetir() {
+
+        ls.add("Daire");
+        ls.add("Rezidans");
+        ls.add("Müstakil");
+        ls.add("Villa");
+        ls.add("Çiftlik Evi");
+        ls.add("Köşk/Konak");
+        ls.add("Yalı");
+        ls.add("Yazlık");
+
+        return ls;
     }
 }
